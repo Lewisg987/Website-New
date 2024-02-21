94f9d93a8642b6c2d0e8d41f93cc3c0b62847fc9
Lewisg987 <146995487+Lewisg987@users.noreply.github.com>
2024-02-21 01:06:20 +0000

    Add files via upload

---
+++ Activities.js
@@ -0,0 +1,49 @@
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.to(".football", {
+    x: 500,
+    duration: 3,
+    scrollTrigger: {
+        start: "80% bottom",
+        end: "30% bottom",
+        scrub: 2,
+        markers: false,
+        toggleActions: "play none none reverse",
+    }
+})
+
+gsap.to(".football", {
+    scale: 2,
+    duration: 3,
+    scrollTrigger: {
+        start: "80% bottom",
+        end: "30% bottom",
+        scrub: 2,
+        markers: true,
+        toggleActions: "play none none reverse",
+    }
+})
+
+gsap.to(".f-info", {
+    scale: 3,
+    duration: 3,
+    scrollTrigger: {
+        start: "80% bottom",
+        end: "30% bottom",
+        scrub: 2,
+        markers: true,
+        toggleActions: "play none none reverse",
+    }
+})
+
+gsap.to(".f-info", {
+    x: -775,
+    duration: 3,
+    scrollTrigger: {
+        start: "80% bottom",
+        end: "30% bottom",
+        scrub: 2,
+        markers: true,
+        toggleActions: "play none none reverse",
+    }
+})
